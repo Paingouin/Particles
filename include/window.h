a98fb13558b8ff46943e265b7971dab1b82cbd46
Paingouin <jordan.bussiere21@gmail.com>
2020-06-15 09:59:31 +0200

    Fix Ubuntu CI #3...

--- include/window.h
+++ include/window.h
@@ -2,9 +2,9 @@
 
 //Using SDL, glew and standard IO
 #include <SDL.h>
-#include <gl\glew.h>
+#include <GL\glew.h>
 #include <SDL_opengl.h>
-#include <gl\glu.h>
+#include <GL\glu.h>
 
 #include <iostream>
 
