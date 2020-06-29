7b7e68372696ba8c36c179e9b6e2f2ee18d5e70b
Paingouin <jordan.bussiere21@gmail.com>
2020-06-29 14:39:59 +0200

    Move to targeted conan lib

---
+++ tools/build2.bat
@@ -0,0 +1,27 @@
+@echo off
+echo ****************************************
+echo * Batch building folder creator V. 1.4 *
+echo ****************************************
+
+REM if exist "%~dp0/Build" (
+REM 	rmdir  /S /Q "%~dp0/Build"
+REM )
+
+cd ..
+mkdir Build_release
+cd Build_release
+
+cmake -H.. -B. -G"Visual Studio 15 2017 Win64"  -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Release 
+)
+
+rem install phase
+cmake --build . --target Particles --config Release
+)
+
+
+rem package phase
+REM cmake --build . --target Package --config Debug
+rem )
+
+
+pause
